d46f4485e847e0027217419a60450eb963be2a00
Danka Proszkiewicz <danka.proszk@gmail.com>
2023-04-11 04:47:00 +0200

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,10 @@
 function fetchCountries(name) {
-    return fetch('https://restcountries.com/v3.1/?fields=name,capital,population,flags,languages')
-  .then(response => response.json())
-  .then(data => data)
-  .catch(error => console.log(error))}
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => response.json())
+    .then(data => data)
+    .catch(error => console.log(error));
+}
 
-export {fetchCountries}
-
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,60 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import Notiflix from 'notiflix';
-import debounce from 'lodash.debounce';
+let debounce = require('lodash.debounce');
 
-const countrySearch = document.querySelector('input');
-const countryList = document.querySelector('country-list');
-const countryInfo = document.querySelector('country-info');
+const inputSearch = document.querySelector('input#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
-// const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 300;
 
+function clearOutput() {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+}
 
+function searchCountry() {
+  clearOutput();
+  let input = inputSearch.value.trim();
+  fetchCountries(input).then(data => {
+    console.log(data);
+    if (data.length > 1 && data.length < 10) {
+      countryList.insertAdjacentHTML(
+        'afterbegin',
+        data
+          .map(element => {
+            return `<li class="country-list__item"><img width="70px" src="${element.flags.svg}"/><p>${element.name.official}<p></li>`;
+          })
+          .join('')
+      );
+    } else if (data.length === 1) {
+      countryInfo.insertAdjacentHTML(
+        'afterbegin',
+        data
+          .map(element => {
+            return `<li class="country-info__item"><h2>${
+              element.name.official
+            }</h2></li><li class="country-info__item"><img width="150" src="${
+              element.flags.svg
+            }"/></li><li class="country-info__item">Capital: ${
+              element.capital
+            }</li><li class="country-info__item">Population: ${
+              element.population
+            }</li><li class="country-info__item">Languages: ${Object.values(
+              element.languages
+            )}</li>`;
+          })
+          .join('')
+      );
+    } else if (data.length > 10) {
+      Notiflix.Notify.info(
+        'Too many matches found. Please enter a more specific name.'
+      );
+    } else if (data.status === 404) {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    }
+  });
+}
 
-// Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
-// Notiflix.Notify.failure("Oops, there is no country with that name")
-
-
-
-// fetch(url, options)
-// trim()
+inputSearch.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
